2f100b2b22f01dbdfcad532650f303bf5d902727
piyushhirwani235 <piyushhirwani235@gmail.com>
2026-07-01 00:55:16 +0530

    Fix React routing and add Vercel SPA rewrite rules

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -50,7 +50,7 @@ const Signup = () => {
         />
         <button type="submit">Sign Up</button>
       </form>
-      <p>Already have an account? <Link to="/login">Login here</Link></p>
+      <p>Already have an account? <Link to="/">Login here</Link></p>
     </div>
   );
 };
